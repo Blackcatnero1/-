a5e5340c05c6bb0f22901c32867f9b37427ad41d
human <human@DESKTOP-21L5O5N>
2024-05-23 20:08:32 +0900

    list 수정

---
+++ boo/src/main/java/com/human/boo/dao/ListDao.java
@@ -0,0 +1,22 @@
+package com.human.boo.dao;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.human.boo.util.PageUtil;
+import com.human.boo.vo.ListVO;
+
+public class ListDao {
+	@Autowired
+	private SqlSessionTemplate session;
+	
+	public List<ListVO> getList(PageUtil page){
+		return session.selectList("lSQL.selList", page);
+	}
+	
+	public int getTotal() {
+		return session.selectOne("fSQL.selTotal");
+	}
+}
--- boo/src/main/java/com/human/boo/vo/ListVO.java
+++ boo/src/main/java/com/human/boo/vo/ListVO.java
@@ -1,6 +1,41 @@
 package com.human.boo.vo;
 
 public class ListVO {
-	private int dealno, objno, ano;
-	private String aptname, dongname;
+	private int dealno, objno;
+	private String aptname, dongname, deal_ymd;
+	public int getDealno() {
+		return dealno;
+	}
+	public void setDealno(int dealno) {
+		this.dealno = dealno;
+	}
+	public int getObjno() {
+		return objno;
+	}
+	public void setObjno(int objno) {
+		this.objno = objno;
+	}
+	public String getAptname() {
+		return aptname;
+	}
+	public void setAptname(String aptname) {
+		this.aptname = aptname;
+	}
+	public String getDongname() {
+		return dongname;
+	}
+	public void setDongname(String dongname) {
+		this.dongname = dongname;
+	}
+	public String getDeal_ymd() {
+		return deal_ymd;
+	}
+	public void setDeal_ymd(String deal_ymd) {
+		this.deal_ymd = deal_ymd;
+	}
+	@Override
+	public String toString() {
+		return "ListVO [dealno=" + dealno + ", objno=" + objno + ", aptname=" + aptname + ", dongname=" + dongname
+				+ ", deal_ymd=" + deal_ymd + "]";
+	}
 }
