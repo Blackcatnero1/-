d79599f7e7d63f1ab210398b438d61c2f09b23a9
kimkwangsup <158257844+kimkwangsup@users.noreply.github.com>
2024-05-24 13:54:43 +0900

    Update views.pydd

--- kimkwangsup/ORACLE BOO 계정, 테이블 만들기/views.py
+++ kimkwangsup/ORACLE BOO 계정, 테이블 만들기/views.py
@@ -29,7 +29,7 @@ def Salesdata(request):
         dsn_tns = cx_Oracle.makedsn('localhost', '1521', service_name='xe')
         engine = create_engine(f'oracle+cx_oracle://boo:12345@{dsn_tns}')
         # 데이터프레임을 Oracle 데이터베이스에 저장
-        sql_df = pd.read_sql('SELECT * FROM sales order by deal_ymd desc', engine).head(100)
+        sql_df = pd.read_sql("SELECT acc_year, sgg_cd, sgg_nm, bjdong_cd, bjdong_nm, land_gbn, land_gbn_nm, LPAD(bonbeon, 4, '0') AS bonbeon, LPAD(bubeon, 4, '0') AS bubeon, bldg_nm, obj_amt, bldg_area, tot_area, floor, right_gbn, cntl_ymd, build_year, house_type, req_gbn, rdealer_lawdnm FROM sales order by deal_ymd desc", engine).head(150)
         df.columns = df.columns.str.lower()
         columns = [
             "acc_year", "sgg_cd", "sgg_nm", "bjdong_cd", "bjdong_nm", 
@@ -38,12 +38,14 @@ def Salesdata(request):
             "cntl_ymd", "build_year", "house_type", "req_gbn", "rdealer_lawdnm"
         ]
         sql_df_filled = sql_df.fillna("")
-
-        mergedf = pd.concat([df[1:150], sql_df_filled[1:100]],axis=0)
+        mergedf = pd.concat([df[1:150], sql_df_filled[1:150]],axis=0)
         finaldf = mergedf.drop_duplicates(columns, keep='first')
         realshape =finaldf.shape[0] - sql_df.shape[0]
         read_df = finaldf[:realshape]
+        read_df['bonbeon'] = read_df['bonbeon'].astype(int)
+        read_df['bubeon'] = read_df['bubeon'].astype(int)
         read_df.to_sql(name='sales', con=engine, if_exists='append', index=None)
+
         return HttpResponse("Data has been successfully inserted into the database.")
     else:
         return HttpResponse(f"Failed to retrieve data: {response.status_code}")
