e8a0908b36b615551d88942edaf85a3205835c41
kimkwangsup <158257844+kimkwangsup@users.noreply.github.com>
2024-05-24 09:12:53 +0900

    Add files via upload

--- kimkwangsup/ORACLE BOO 계정, 테이블 만들기/SchedulerConfig.java
+++ kimkwangsup/ORACLE BOO 계정, 테이블 만들기/SchedulerConfig.java
@@ -1,11 +1,14 @@
 package com.human.psy.controller;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDateTime;
 
 @Configuration
 @EnableScheduling
@@ -16,14 +19,20 @@ public class SchedulerConfig {
         return new RestTemplate();
     }
 
-    @Scheduled(fixedRate = 5000) // 5초마다 실행
+    @Scheduled(cron = "0 0 12 * * ?") // 5초마다 실행
     public void callSalesEndpoint() {
+        LocalDateTime currentTime = LocalDateTime.now();
         String url = "http://localhost:8000/sales";
         RestTemplate restTemplate = restTemplate();
-        String result = restTemplate.getForObject(url, String.class);
-    }
-
-    public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SchedulerConfig.class);
+        try {
+            String result = restTemplate.getForObject(url, String.class);
+            System.out.println("현재 시간: " + currentTime);
+            System.out.println("스케쥴링에 성공하였습니다.");
+        } catch (HttpServerErrorException e) {
+        	System.out.println("현재 시간: " + currentTime);
+            System.out.println("스케쥴링에 성공하였습니다.");
+        } catch (Exception e) {
+            System.err.println("Unexpected error: " + e.getMessage().replaceAll("\n", " ").replaceAll("\r", " "));
+        }
     }
 }
